92c90b9d118acb402a5cd5fc08eebe2d74841fcf
Ouail Amarir <ouailamarir@gmail.com>
2025-01-31 14:07:06 +0100

    Ajout du serializer TransactionSerializer et création de la view TransactionViewSet pour gérer les transactions. Implémentation d'une view pour générer la balance comptable au format CSV.

--- backend/accounting_backend/transactions/views.py
+++ backend/accounting_backend/transactions/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+import csv
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+# View pour gérer les transactions
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+# View pour générer la balance comptable
+@api_view(['GET'])
+def generate_balance(request, year):
+    transactions = Transaction.objects.filter(date__year=year)
+    categories = {}
+
+    for transaction in transactions:
+        if transaction.categorie not in categories:
+            categories[transaction.categorie] = 0
+        categories[transaction.categorie] += transaction.montant
+
+    # Cela créer un fichier CSV
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="balance_{year}.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Catégorie', 'Total'])
+    for categorie, total in categories.items():
+        writer.writerow([categorie, total])
+
+    return response
