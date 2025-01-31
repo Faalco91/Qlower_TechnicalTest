df0bc4fdce19c450a1a2ca35c3b18c0d9fc3aa1e
Ouail Amarir <ouailamarir@gmail.com>
2025-01-31 11:13:37 +0100

    Changement de la configuration de la base de données de SQLite à PostgreSQL avec les paramètres appropriés.

--- backend/accounting_backend/accounting_backend/settings.py
+++ backend/accounting_backend/accounting_backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'accounting_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qlower_db',
+        'USER': 'qlower_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
