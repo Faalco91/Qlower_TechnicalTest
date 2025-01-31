0a8ac11ddd30d3d90796d483502d3c7b2d123069
Ouail Amarir <ouailamarir@gmail.com>
2025-01-31 15:45:20 +0100

    Retour en Anglais de la langue et du fuseau horaire dans la configuration du projet Django

--- backend/accounting_backend/accounting_backend/settings.py
+++ backend/accounting_backend/accounting_backend/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Paris'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
