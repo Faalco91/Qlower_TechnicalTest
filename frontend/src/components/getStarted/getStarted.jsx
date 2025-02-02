6733d2b2b55dcbd169f1cff4a43a67827ffc6d1a
Ouail Amarir <ouailamarir@gmail.com>
2025-02-02 22:12:19 +0100

    Réorganisation du composant GetStarted : ajout d'une gestion d'état pour le succès des transactions et amélioration de la mise en page avec une nouvelle structure CSS.

--- frontend/src/components/getStarted/getStarted.jsx
+++ frontend/src/components/getStarted/getStarted.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import "./getStarted.css";
 import BasicModal from "../modal/modal";
 import TransactionForm from "../TransactionForm/TransactionForm";
 import TransactionList from "../TransactionList/TransactionList";
+import financeImg from "../../assets/qlower-finance-img.webp";
 
 const GetStarted = () => { 
+    const [success, setSuccess] = useState(false);
 
     const newTransactionButtonStyle = {
         backgroundColor: '#ECEDEE',
@@ -38,19 +41,26 @@ const GetStarted = () => {
 
     return (
         <section className="get-started-container">
-            <div className="slogan">
-                <h1>Transactions Simplifiées, Finances Maîtrisées</h1>
-                <h1></h1>
-                <p>Gérez et suivez vos transactions, générez des rapports en temps réel et téléchargez votre balance comptable en toute simplicité.</p>
-            </div>
-            <div className="ctas-container">
-                <BasicModal title="Nouvelle transaction" buttonLabel="Saisir une transaction" buttonStyle={newTransactionButtonStyle} className="cta-modal new-transaction">
-                    <TransactionForm />
-                </BasicModal>
+            <div className="slogan-container">
+                <div className="slogan">
+                    <div></div>
+                    <h1>Transactions Simplifiées, Finances Maîtrisées</h1>
+                    <h1></h1>
+                    <p>Gérez et suivez vos transactions, générez des rapports en temps réel et téléchargez votre balance comptable en toute simplicité.</p>
+                </div>
+                <div className="ctas-container">
+                    <BasicModal title="Nouvelle transaction" buttonLabel="Saisir une transaction" buttonStyle={newTransactionButtonStyle} className="cta-modal new-transaction" success={success}>
+                        <TransactionForm setSuccess={setSuccess} />
+                    </BasicModal>
 
-                <BasicModal title="Liste des transactions" buttonLabel="Lister les transactions" buttonStyle={transactionListButtonStyle} className="cta-modal transaction-list">
-                    <TransactionList />
-                </BasicModal>
+                    <BasicModal title="Liste des transactions" buttonLabel="Lister les transactions" buttonStyle={transactionListButtonStyle} className="cta-modal transaction-list">
+                        <TransactionList />
+                    </BasicModal>
+                </div>
+            </div>
+
+            <div className="finance-img-container">
+                <img src={financeImg} alt="Illustration d'une personne qui gère ses finances" />
             </div>
         </section>
     );
